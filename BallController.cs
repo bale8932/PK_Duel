f35eca8161e0b4d00c50a8a8187c8dc29c380e1f
bale8932 <33545322+bale8932@users.noreply.github.com>
2018-04-07 23:15:31 +0900

    Add files via upload

---
+++ BallController.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour
+{
+    public AudioClip shootSE;
+    public AudioClip powerSE;
+    public AudioClip whistleSE;
+    AudioSource aud;
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+    private int kicked = 0;//コルーチンが終了するまで、新たなコルーチンを開始させないためのフラグ
+    private int ctrl = 0;//シュートの方向を変えるためのフラグ
+   
+
+    
+    void Start()
+    {
+        this.aud = GetComponent<AudioSource>();
+        // Shoot(new Vector3(800, 200, 0));
+        this.aud.PlayOneShot(this.whistleSE);
+    }
+
+    void Update()
+    {    
+        if(Input.GetKeyDown("right"))
+        {
+            this.ctrl = 1;
+        }
+        if (Input.GetKeyDown("left"))
+        {
+            this.ctrl = 2;
+        }
+        if (this.kicked == 0 && this.ctrl == 1 && Input.GetKeyDown("up"))
+        {
+            StartCoroutine("Right");
+        }
+        if (this.kicked == 0 && this.ctrl == 2 && Input.GetKeyDown("up"))
+        {
+            StartCoroutine("Left");
+        }
+        if (this.kicked == 0 && this.ctrl == 0 && Input.GetKeyDown("up"))
+        {
+            StartCoroutine("Up");
+        }
+        if (this.kicked == 0 && this.ctrl == 0 && Input.GetKeyDown("down"))
+        {
+            StartCoroutine("Down");
+        }
+        if (this.kicked == 0 && this.ctrl == 1 && Input.GetKeyDown("down"))
+        {
+            StartCoroutine("PowerRight");
+        }
+        if (this.kicked == 0 && this.ctrl == 2 && Input.GetKeyDown("down"))
+        {
+            StartCoroutine("PowerLeft");
+        }
+
+    }
+    IEnumerator Right()
+        {
+        this.kicked = 1;
+        yield return new WaitForSeconds(1.2f);
+            Shoot(new Vector3(800, 200, -300));
+        this.aud.PlayOneShot(this.shootSE);
+
+    }
+    IEnumerator Left()
+    {
+        this.kicked = 1;
+        yield return new WaitForSeconds(1.2f);
+        Shoot(new Vector3(800, 200, 300));
+        this.aud.PlayOneShot(this.shootSE);
+    }
+    IEnumerator Up()
+    {
+        this.kicked = 1;
+        yield return new WaitForSeconds(1.2f);
+        Shoot(new Vector3(800, 200, 0));
+        this.aud.PlayOneShot(this.shootSE);
+    } 
+    
+    IEnumerator Down()
+    {//  外れる可能性があるが、GKが防ぎにくいシュートを放つ
+        this.kicked = 1;
+        yield return new WaitForSeconds(1.2f);
+        float y = Random.Range(1, 5);
+        Shoot(new Vector3(1000, 150 * y, 0)); // y=4のときシュートが外れる
+        this.aud.PlayOneShot(this.powerSE);
+        Rigidbody rb = GetComponent<Rigidbody>();
+//  オブジェクトの Rigidbody の Mass をを増加させてゴールを揺らすシュートを打つ
+        rb.mass = 2000f;
+    }
+    IEnumerator PowerRight()
+    {//  外れる可能性があるが、GKが防ぎにくいシュートを放つ
+        this.kicked = 1;
+        yield return new WaitForSeconds(1.2f);
+        float z = Random.Range(1, 4);
+        Shoot(new Vector3(1100, 200 , -250 * z)); // z=3のときシュートが外れる
+        this.aud.PlayOneShot(this.powerSE);
+        Rigidbody rb = GetComponent<Rigidbody>();
+        //  オブジェクトの Rigidbody の Mass をを増加させて
+        rb.mass = 2000f;
+    }
+    IEnumerator PowerLeft()
+    {//  外れる可能性があるが、GKが防ぎにくいシュートを放つ
+        this.kicked = 1;
+        yield return new WaitForSeconds(1.2f);
+        float u = Random.Range(1, 4);
+        Shoot(new Vector3(1100, 200, 250 * u)); // y=uのときシュートが外れる
+        this.aud.PlayOneShot(this.powerSE);
+        Rigidbody rb = GetComponent<Rigidbody>();
+       
+        rb.mass = 5000f;
+    }
+}
+
---
+++ ComKickController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class ComKickController : MonoBehaviour {
+    public AudioClip shootSE;
+    public AudioClip powerSE;
+    public AudioClip whistleSE;
+    AudioSource aud;
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+    public GameObject gameobject;
+    private int comkicked = 0; //コルーチンが終了するまで、新たなコルーチンを開始させないためのフラグ
+    int ratio = 2; //COMが強力力シュートを打つ確率20%
+    int ration = 1; //COMが真ん中に強力力シュートを打つ確率10%
+    void Start()
+    {
+        this.aud = GetComponent<AudioSource>();
+        this.gameobject = GameObject.Find("GameDirector");
+        StartCoroutine("ComShoot");
+        this.aud.PlayOneShot(this.whistleSE);
+    }
+   
+	// Update is called once per frame
+	void Update () {
+      
+            
+      
+    }
+    public IEnumerator ComShoot()
+    {
+        
+        yield return new WaitForSeconds(3.4f);
+        int dice = Random.Range(1, 11);
+        if(dice <= this.ratio) {
+            
+            float r = Random.Range(-3, 4);
+            Shoot(new Vector3(1000, 200 , 200 * r));
+            this.aud.PlayOneShot(this.powerSE);
+            Rigidbody rb = GetComponent<Rigidbody>();
+  //  オブジェクトの Rigidbody の Mass を増加させてゴールを揺らすシュートを打つ
+            rb.mass = 2000f;
+        } else if (dice == this.ration){
+        float y = Random.Range(1, 5);
+        Shoot(new Vector3(800, 150 * y, 300));
+        this.aud.PlayOneShot(this.powerSE);
+        }
+        else
+        {
+            float z = Random.Range(-1, 2);
+            Shoot(new Vector3(800, 200, 300 * z));
+            this.aud.PlayOneShot(this.shootSE);
+        }
+        yield return new WaitForSeconds(3.2f);
+        if (this.gameobject.GetComponent<GameDirector>().point == 3 && this.gameobject.GetComponent<GameDirector>().lose == 0 
+           || this.gameobject.GetComponent<GameDirector>().match == 4 && this.gameobject.GetComponent<GameDirector>().point - this.gameobject.GetComponent<GameDirector>().lose == 2 
+           || this.gameobject.GetComponent<GameDirector>().match > 4 && this.gameobject.GetComponent<GameDirector>().lose < this.gameobject.GetComponent<GameDirector>().point)
+        {
+            SceneManager.LoadScene("WIN");
+            yield break;
+       } else if (this.gameobject.GetComponent<GameDirector>().point == 0 && this.gameobject.GetComponent<GameDirector>().lose == 3 
+            || this.gameobject.GetComponent<GameDirector>().match == 4 && this.gameobject.GetComponent<GameDirector>().lose - this.gameobject.GetComponent<GameDirector>().point == 2
+            || this.gameobject.GetComponent<GameDirector>().match > 4 && this.gameobject.GetComponent<GameDirector>().point < this.gameobject.GetComponent<GameDirector>().lose)
+        {
+            SceneManager.LoadScene("LOSE");
+            yield break;
+        }
+        this.gameobject.GetComponent<GameDirector>().GetShoot();
+        SceneManager.LoadScene("pk2");
+    }
+}
+
